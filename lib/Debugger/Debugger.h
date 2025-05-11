9c4bad7bc019c49f7f19939e056c4c49f4b363f0
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-05-11 11:27:52 -0300

    Move Debugger file; implemented Serial and log to serial method

--- lib/Debugger/Debugger.h
+++ lib/Debugger/Debugger.h
@@ -1,5 +1,8 @@
+#pragma once
 #ifndef Debugger_h
 #define Debugger_h
+
+#include <Arduino.h>
 
 class Debugger
 {
@@ -7,8 +10,10 @@ protected:
   bool debugger_enable;
 
 public:
-  void _enableDebugger(bool enable);
+  Debugger(bool enable);
   bool _isDebuggerEnable();
+  void init();
+  void logToSerial(String msg);
 };
 
 #endif
